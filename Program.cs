9c5681a4a89c2fbcbde9ba431f690c65a6b7e3ce
Ave Pajo <ave.pajo@tktk.ee>
2024-12-16 13:22:36 +0200

    Week7Functions

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string hello = "Hello, world!";
+
+string panic = "Don´t panic!";
+
+//for(int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine(hello);
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine(panic);
+//}
+
+PrintStringFiveTimes(hello);
+PrintStringFiveTimes(panic);
+PrintStringFiveTimes("Ave");
+
+static void PrintStringFiveTimes(string someString)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(someString);
+    }
+}
